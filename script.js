fe13442b0ea1d8743d1a98bf89b5121fe016ccbc
Trezentos <gustavofagundes1998@hotmail.com>
2020-06-06 18:39:57 -0300

    Conectando com o github

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const body = document.querySelector('body')
+const div = document.querySelector('div')
+const colors = ['deepskyblue', '#239B56 ', '#2E4053', '#0E6251','violet'
+                ,'#2E4053', 'purple', 'tomato', 'yellow']
+
+
+document.querySelector('div').addEventListener('click', changeCollor)
+
+function changeCollor(){
+
+    let random = Math.ceil( Math.random()*colors.length )-1
+    
+
+    body.style.backgroundColor = colors[random]
+    div.style.backgroundColor= colors[random]
+    console.log(random)
+    
+}
+
